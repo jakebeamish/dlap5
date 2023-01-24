61ccbfb4ae839e33b25f1ab631aa19c154cbe2d5
Jake Beamish <jakebeamish@gmail.com>
2023-01-24 11:28:05 +0000

    Make it full page with small dots

--- sketch.js
+++ sketch.js
@@ -1,38 +1,47 @@
 let body = [];
-
 let grain;
-
-let r = 2;
+let r = 1.5;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
     body[0] = createVector(width / 2, height / 2);
 }
 
 function draw() {
     background(0);
+    noStroke();
 
-    
-    for (g of body) {
-        if (dist(g.x, g.y, width/2, height/2) > 100) {
-            noLoop();
-        }
-        fill(255, 100);
-        circle(g.x, g.y, r * 2);
-    }
 
-    for (let a = 0; a < 3000; a++) {
+    for (let a = 0; a < 1000; a++) {
+        // let dice = floor(random(4));
+        // let dice = 0;
+        // if (dice === 0) {
+            // grain = createVector(random(width), 0);
+        // } else if (dice === 1) {
+            // grain = createVector(random(width), height);
+        // }
+        // grain.add(p5.Vector.random2D().mult(200))
         grain = createVector(random(width), random(height));
-
-        // stroke(128);
-        // circle(grain.x, grain.y, r * 2);
+        fill(128, 50);
+        circle(grain.x, grain.y, r * 2);
         for (let i = 0; i < body.length; i++) {
             let distance = p5.Vector.dist(grain, body[i]);
             if (distance < r * 2) {
                 body.push(grain);
                 break;
+            } else {
+
             }
         }
+    }
+
+    for (g of body) {
+        if (dist(g.x, g.y, width / 2, height / 2) > 150) {
+            noLoop();
+        }
+        fill(255, 50);
+        stroke(255, 50);
+        circle(g.x, g.y, r * 2);
     }
 
 }
